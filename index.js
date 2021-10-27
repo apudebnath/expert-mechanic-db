1d4f4aa9111157d8c503f81f97e9de5334c09f6f
apudebnath <apu1578@gmail.com>
2021-10-27 19:43:50 +0600

    content updated

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-//user: geniuscaruser 
-//password: NDYWyw4xAlwAoleW
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS }@cluster0.mthak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri)
@@ -43,10 +42,8 @@ async function run(){
         // POST API
         app.post('/services', async(req, res) => {
             const service = req.body;
-           console.log('Post api hitted', service)
-
+           console.log('Post Api hitted', service)
             const result = await servicesCollection.insertOne(service);
-            console.log(result)
             res.json(result)
         });
         //DELETE API
@@ -68,5 +65,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Running Genius Server on port', port)
+    console.log('Running MY Genius Server on port', port)
 })
